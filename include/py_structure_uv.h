ab2ad279204dd8f35a9662813c5cbb056f35b897
김건호 <74143636+secripite@users.noreply.github.com>
2026-08-20 08:56:07 +0000

    Add header file for create project function
    - Feat: add create_project.h, py_structure_uv.h for function declarations
    - Refactor: modify config.h, and main.c to include new header files

---
+++ include/py_structure_uv.h
@@ -0,0 +1,9 @@
+#ifndef PY_STRUCTURE_UV_H
+#define PY_STRUCTURE_UV_H
+
+#include "config.h" 
+
+//setup_python_uv_project: Initializes a Python project using the uv tool based on the provided configuration.
+void setup_python_uv_project(GimbapConfig *config);
+
+#endif
