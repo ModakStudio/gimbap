035430a06bdc97571d21db045f8a914443eab83e
김건호 <74143636+secripite@users.noreply.github.com>
2026-08-25 15:19:34 +0900

    Implement dynamic Python UV initializer and modularize project creation structure (#5)
    * Feat: added project creation function for python with uv framework
    
    - feat: create project directory dynamically and initialize base template via 'uv init'
    - feat: prompt user interactively to pin python version using 'uv python pin'
    - feat: sync isolated virtual environment (.venv) and lock dependencies using 'uv sync'
    - feat: clean up default README.md based on config->readme TUI configuration
    
    * Feat: added create_project function
    
    - Feat: added function that chooses the appropriate project setup function based on the language from config
    
    * Add header file for create project function
    
    - Feat: add create_project.h, py_structure_uv.h for function declarations
    - Refactor: modify config.h, and main.c to include new header files
    
    * Fix : fix bug that create_project cannot find the function in py_structure_uv.c
    
    * Refactor : update .gitignore to ignore obj dir
    
    * Feat : add logic for checking if the version selected is valid or not

---
+++ include/create_project.h
@@ -0,0 +1,9 @@
+#ifndef CREATE_PROJECT_H
+#define CREATE_PROJECT_H
+
+#include "config.h"
+
+// create_project: call the appropriate project setup function based on the selected language in the configuration.
+void create_project(GimbapConfig *config);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdbool.h>
 #include "config.h"
 #include "tui.h"
+#include "create_project.h"
 
 int main() {
     GimbapConfig config = {"", 0, false, 0, false};
 
     tui_run(&config);
+    create_project(&config);
 
     return 0;
 }
