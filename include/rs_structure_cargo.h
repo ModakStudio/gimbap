5c0e99ba532abc003e78da6ef746ce0ce937f601
김건호 <74143636+secripite@users.noreply.github.com>
2026-09-04 07:39:05 +0000

    Feat : Add rust function to main function
    -feat: add rs_structure_cargo.h
    -feat: add setup_rust function into create_project.c

---
+++ include/rs_structure_cargo.h
@@ -0,0 +1,9 @@
+#ifndef RS_STRUCTURE_CARGO_H
+#define RS_STRUCTURE_CARGO_H
+
+#include "config.h" 
+
+//setup_rust_cargo_project: Initializes a Rust project using the cargo tool based on the provided configuration.
+void setup_rust_cargo_project(GimbapConfig *config);
+
+#endif
